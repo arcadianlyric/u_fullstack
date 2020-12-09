be0e23900d1403dc3dffa6fd80a0979a3bac14dc
Ying <yingcai1123@gmail.com>
2020-12-09 11:54:24 -0800

    init

--- projects/capstone/starter/app.py
+++ projects/capstone/starter/app.py
@@ -2,12 +2,94 @@ import os
 from flask import Flask, request, abort, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
+import json
+from .database.models import db_drop_and_create_all, setup_db, dish
+from .auth.auth import AuthError, requires_auth
 
 
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__)
     CORS(app)
+    '''
+    Get ingradients by dishes
+    Get dishes by ingradients
+    '''
+    
+    @app.route('/dishes', methods=['GET'])
+    def get_dishes():
+        results = {
+        "success": True,
+        "dishes": [dish.short() for dish in Dish.query.all()]
+    }
+    return jsonify(results)
+
+
+    @app.route("/dishes-detail", methods=["GET"])
+    @requires_auth("get:dishes-detail")
+    def get_dishes_detail(jwt):
+        results = {
+            "success": True,
+            "dishes": [dish.long() for dish in dish.query.all()]
+        }
+        return jsonify(results)
+
+
+    @app.route("/dishes", methods=["POST"])
+    @requires_auth("post:dishes")
+    def post_dish(jwt):
+        """Adds a dish. Requires authentication and permission"""
+        body = request.get_json()
+        if not body:
+            abort(400)
+
+        dish = Dish(name=body["name"], recipe=json.dumps(body["recipe"]))
+        dish.insert()
+        results = {
+            "success": True,
+            "dishes": [dish.long()]
+        }
+        return jsonify(results)
+
+
+    @app.route("/dishes/<int:dish_id>", methods=["PATCH"])
+    @requires_auth("patch:dishes")
+    def patch_dishes(jwt, dish_id):   # jwt must be the 1st
+        dish = dish.query.get(dish_id)
+        if not dish:
+            abort(404)
+
+        body = request.get_json()
+
+        if not body or ("name" not in body and "recipe" not in body):
+            abort(400)
+
+        if "name" in body:
+            dish.name = body["name"]
+        if "recipe" in body:
+            dish.recipe = json.dumps(body["recipe"])
+        dish.update()
+
+        results = {
+            "success": True,
+            "dishes": [dish.long()]
+        }
+        return jsonify(results)
+
+
+    @app.route("/dishes/<int:dish_id>", methods=["DELETE"])
+    @requires_auth("delete:dishes")
+    def delete_dishes(jwt, dish_id):
+        dish = Dish.query.get(dish_id)
+        if not dish:
+            abort(404)
+
+        dish.delete()
+        return jsonify({
+            "success": True,
+            "delete": dish_id
+            })    
+
 
     # Error Handling
     @app.errorhandler(400)
